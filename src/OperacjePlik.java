c6fdce2db0b70d1085ad4cfc57f42089c256b0bc
Adrian Iwañski <adriadniwan1@gmail.com>
2021-01-24 21:06:09 +0100

    Stworzenie metod ZApisywanie i Odczytywanie w klasie Ksiazka

--- src/OperacjePlik.java
+++ src/OperacjePlik.java
@@ -9,16 +9,43 @@ public class OperacjePlik{
     public void Zapisywanie(Ksiazka NowaKsiazka) throws IOException {
         RandomAccessFile baza =new RandomAccessFile("Books.bin","rw");
 
-        baza.writeInt(NowaKsiazka.GetIdKsiazki());
-        baza.writeUTF(NowaKsiazka.GetNazwaKsiazki());
-        baza.writeUTF(NowaKsiazka.GetAutor());
-        baza.writeUTF(NowaKsiazka.GetGatunek());
-        baza.writeUTF(NowaKsiazka.GetDataWydania());
-        baza.writeUTF(NowaKsiazka.GetWyporzyczajacy());
-        baza.writeUTF(NowaKsiazka.GetGatunek());
-        //test
+        baza.seek(baza.length());
+
+        baza.writeInt(NowaKsiazka.GetIdKsiazki()); // 32
+        baza.writeUTF(String.format("%1$-32s",NowaKsiazka.GetNazwaKsiazki()));
+        baza.writeUTF(String.format("%1$-32s",NowaKsiazka.GetAutor()));
+        baza.writeUTF(String.format("%1$-32s",NowaKsiazka.GetGatunek()));
+        baza.writeUTF(String.format("%1$-10s",NowaKsiazka.GetDataWydania()));
+        baza.writeUTF(String.format("%1$-32s",NowaKsiazka.GetWyporzyczajacy()));
+
+        baza.writeInt(NowaKsiazka.GetDniWyporzyczenia());
+        baza.writeInt(NowaKsiazka.GetMiesiaceWyporzyczenia());
+        baza.writeInt(NowaKsiazka.GetRokWyporzyczenia());
+        baza.writeBoolean(NowaKsiazka.GetCzyWyporzyczona()); //1
+
+        baza.writeInt(NowaKsiazka.GetDniTermin());
+        baza.writeInt(NowaKsiazka.GetMiesiaceTermin());
+        baza.writeInt(NowaKsiazka.GetRokTermin());
+        baza.writeBoolean(NowaKsiazka.GetCzyPoTerminie()); //1
+
+        System.out.println("ZApis udany");
 
 
     }
+
+    public Ksiazka Odczytywanie() throws IOException {
+        RandomAccessFile baza =new RandomAccessFile("Books.bin","rw");
+        baza.seek(0);
+
+        Ksiazka OdczytywanaKsiazka= new Ksiazka(baza.readInt(),baza.readUTF(),baza.readUTF(), baza.readUTF(), baza.readUTF(), baza.readUTF(),baza.readInt(), baza.readInt(), baza.readInt(), baza.readBoolean(), baza.readInt(), baza.readInt(), baza.readInt(), baza.readBoolean());
+        baza.seek(364);
+
+        return OdczytywanaKsiazka;
+
+    }
+
+
+
+
 
 }
